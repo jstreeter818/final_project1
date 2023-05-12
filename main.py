28093e0912fc4e1c95568835d1cca51f0c11ff69
cjohnson642 <coltonjohnson@unomaha.edu>
2023-05-12 14:20:22 -0500

    fixed name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gui import *
 
 def main():
     window = Tk()
-    window.title('Shape Area Calculator')
+    window.title('Shape Calculator')
     window.geometry('700x425')
     window.resizable(False, False)
 
